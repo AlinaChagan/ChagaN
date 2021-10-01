f698212c54184beb252fb1a3717de908b5cc7c26
AlinaChagan <alina.chagan@yandex.ru>
2021-10-01 10:37:24 +0300

    Add my project

---
+++ Laba 1/лаба 1 доп 1/лаба 1 доп 1/лаба 1 доп 1.cpp
@@ -0,0 +1,73 @@
+﻿//Функция, которая находит все простые числа из нескольких интервалов. Интервалы задаются границами a и b.
+//Программа должна содержать функцию пользователя с переменным числом параметров и не менее трех обращений к ней с различным количеством параметров.
+
+#include <iostream>
+using namespace std;
+bool checkprost(int);
+void diapazon1(int, int);
+void prost(int, int*, ...);
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int k;
+	cout << "Введите количество интервалов для проверки (не более 6): ";
+	cin >> k;
+	if (k <= 0 || k > 6)
+	{
+		cout << "Введенные данные некорректны. ";
+		return 0;
+	}
+	int** A = new int* [k];
+	for (int i = 0; i < k; i++) A[i] = new int[2];
+	for (int i = 0; i < k; i++)
+	{
+		cout << "Введите левую границу " << i + 1 << " интервала: ";
+		cin >> A[i][0];
+		cout << "Введите правую границу " << i + 1 << " интервала: ";
+		cin >> A[i][1];
+	}
+	if (k == 1) prost(1, A[0]);
+	else if (k == 2) prost(2, A[0], A[1]);
+	else if (k == 3) prost(3, A[0], A[1], A[2]);
+	else if (k == 4) prost(4, A[0], A[1], A[2], A[3]);
+	else if (k == 5) prost(5, A[0], A[1], A[2], A[3], A[4]);
+	else if (k == 6) prost(6, A[0], A[1], A[2], A[3], A[4], A[5]);
+
+	//высвободим динамическую память
+	for (int i = 0; i < k; i++) delete[] A[i];
+	delete[] A;
+	return 0;
+}
+bool checkprost(int q)
+{
+	//проверяет, является ли число простым, возвращает р-т проверки 
+	bool result = true;
+	for (int i = 2; i <= sqrt(q); i++)
+	{
+		if (q % i == 0)
+		{
+			result = false;
+			break;
+		}
+	}
+	return result;
+}
+void diapazon1(int a, int b)
+{
+	//выводит все простые числа на интервале [a;b]
+	for (int p = a; p <= b; p++)
+	{
+		if (checkprost(p)) cout << p << " ";
+	}
+	cout << endl;
+}
+void prost(int n, int* p1, ...)
+{
+	//первым параметром принимает количество интервалов для проверки
+	int** pp = &p1;
+	for (int i = 1; i <= n; i++)
+	{
+		diapazon1((*pp)[0], (*pp)[1]);
+		pp++;
+	}
+}
---
+++ Laba 1/лаба 1 основа/лаба 1 основа/лаба 1 основа.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <math.h>
+	using namespace std;
+	float mdichotom(float (*) (float), float, float, float); //первым параметром передается адрес рабочей функции (прототип(описание, объявление) ф-ии mdichotom)
+	float func1(float); //функция №1
+	float func2(float); //функция №2
+	void func3(float*, float*); //функция получения значений a и b с клавиатуры 
+	int main()
+	{
+		setlocale(LC_CTYPE, "Russian");
+		int n;
+		float a, b;
+		float e = 0.001f;
+		cout << "Выберите функцию f(x) : \n\n 1:  e^x + 2x^2 – 3 \n 2:  x^3 + 3 " << endl;
+		cin >> n;
+		switch (n)
+		{
+		   case 1:
+		   {
+			func3(&a, &b);
+			cout << "Отделенный корень уравнения для функции №1  = " << mdichotom(func1, e, a, b) << endl;
+			break;
+		   }
+		   case 2:
+		   {
+		    func3(&a, &b);
+			cout << "Отделенный корень уравнения для функции №2 = " << mdichotom(func2, e, a, b) << endl;
+			break;
+		   }
+		   default:
+		   {
+			cout << "Введенные данные некорректны." << endl;
+			break;
+		   }
+		} //switch
+
+		return 0;
+	}
+
+	float func1(float x) //определение ф-ии func1
+	{
+		return sin(x) + pow(x, 3);
+	}
+	float func2(float x) //определение ф-ии func2
+	{
+		return (float)(0.4 + pow(x, 3));
+	}
+
+	float mdichotom(float (*f1) (float), float e, float a, float b)
+	{
+		float x;
+		do
+		{
+			x = (a + b) / 2;
+			if ((f1(x)) * (f1(a)) <= 0)
+				b = x;
+			else
+				a = x;
+
+		} while ((abs(a - b)) > 2 * e);
+		return x;
+	}
+	void func3(float* var1, float* var2)
+	{
+		cout << "Введите a = ";
+		cin >> *var1;
+		cout << endl;
+		cout << "Введите b = ";
+		cin >> *var2;
+		cout << endl;
+	}
+
+
+
